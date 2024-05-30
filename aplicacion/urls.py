a37d88e41fd9a2bbd9b073e23b8cc9df85f42ba0
brian <bria.chavez@duocuc.cl>
2024-05-29 20:51:28 -0400

    se hicieron los urls de aplicacion y se aplicaron statics

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,6 +1,36 @@
 
 from django.urls import path
-from .views import index
+from .views import index, beretta, camisa, carpa, casco, chaleco, cuchillo, pantalon, valken, about, admin, cart, checkout, estado
+from .views import miscompras, panelcerrarsesion, panelcontrol, panelcontrolagregararmas,panelcontroleditararmas, panelcontrolestadocompra
+from .views import panelcontrolusuarios, punitario, registro, sesion, shop, thankyou
+
+#URLS DE APLICACION
 urlpatterns = [
        path('', index, name='index'),
+       path('producto/beretta', beretta, name='beretta'),
+       path('producto/camisa', camisa, name='camisa'),
+       path('producto/carpa', carpa, name='carpa'),
+       path('producto/casco', casco, name='casco'),
+       path('producto/chaleco', chaleco, name='chaleco'),
+       path('producto/cuchillo', cuchillo, name='cuchillo'),
+       path('producto/pantalon', pantalon, name='pantalon'),
+       path('producto/valken', valken, name='valken'),
+       path('about/', about, name='about'),
+       path('admin/', admin, name='admin'),
+       path('cart/', cart, name='cart'),
+       path('checkout/', checkout, name='checkout'),
+       path('estado/', estado, name='estado'),
+       path('miscompras/', miscompras, name='miscompras'),
+       path('panelcerrarsesion/', panelcerrarsesion, name='panelcerrarsesion'),
+       path('panelcontrol/', panelcontrol, name='panelcontrol'),
+       path('panelcontrolagregararmas/', panelcontrolagregararmas, name='panelcontrolagregararmas'),
+       path('panelcontroleditararmas/', panelcontroleditararmas, name='panelcontroleditararmas'),
+       path('panelcontrolestadocompra/', panelcontrolestadocompra, name='panelcontrolestadocompra'),
+       path('panelcontrolusuarios/', panelcontrolusuarios, name='panelcontrolusuarios'),
+       path('punitario/', punitario, name='punitario'),
+       path('registro/', registro, name='registro'),
+       path('sesion/', sesion, name='sesion'),
+       path('shop/', shop, name='shop'),
+       path('thankyou/', thankyou, name='thankyou'),
+       
 ]
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#URLS DE PROYECTO
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aplicacion.urls')),
