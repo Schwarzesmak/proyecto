708741c4d8dc799b4924208eae1953e84016b43f
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-06-14 20:43:58 -0400

    Se estan haciendo cambios en Forms

--- aplicacion/forms.py
+++ aplicacion/forms.py
@@ -2,30 +2,26 @@ from django import forms
 from .models import Persona, Producto
 
 #Esto es para los formularios #falta completar funciones
-class PersonaForm(forms.ModelForm):
-    
-    #VER CRUD DEL PROFE PARA TERMINAR 
-    class Meta:
-        model = Persona
-        fields = []
-    
-class UpdatePersonaForm(forms.ModelForm):
-    
-    #VER CRUD DEL PROFE PARA TERMINAR 
-    class Meta:
-        model = Persona
-        fields = []
-        
 class ProductoForm(forms.ModelForm):
-    
+    nombre = forms.CharField(max_length=50, error_messages= {"required":"Ingrese su nombre"}, 
+                        help_text="Debe ingresar su nombre")
+    tipo_producto = forms.CharField(max_length=50, error_messages= {"required":"Ingrese el tipo de producto"}, 
+                        help_text="Debe ingresar que tipo de producto es")
+    tipo_municion = forms.CharField(max_length=35, error_messages= {"required":"Ingrese el tipo de municion"}, 
+                        help_text="Debe ingresar que tipo de  es")
+    precio = forms.IntegerField(max_length="20", error_messages= {"required":"Ingrese el precio"}, 
+                        help_text="Debe ingresar el valor del producto")
     #VER CRUD DEL PROFE PARA TERMINAR 
     class Meta:
         model = Persona
-        fields = []
-        
+    fields = ['cod_producto',
+              'nombre', 
+              'tipo_producto',
+              'tipo_municion']
+
 class UpdateProductoForm(forms.ModelForm):
     
-     #VER CRUD DEL PROFE PARA TERMINAR 
+    #VER CRUD DEL PROFE PARA TERMINAR 
     class Meta:
         model = Persona
         fields = []
