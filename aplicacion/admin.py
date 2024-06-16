7bb92bdaa982c3477c300acc0ed9e0234563c165
brian <bria.chavez@duocuc.cl>
2024-06-16 19:25:42 -0400

    asdasd

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Persona, Usuario, Envio, Producto, Carrito
+from .models import Persona, Usuario, Envio, Producto, Carrito, Pedido
 # Register your models here.
 
 class AdmPersona(admin.ModelAdmin):
@@ -17,19 +17,28 @@ class AdmEnvio(admin.ModelAdmin):
     list_filter = ['fecha_compra', 'estado', 'usuario']
     
 class AdmProducto(admin.ModelAdmin):
-    list_display = ['nombre', 'tipo_producto', 'tipo_municion', 'precio', 'stock', 'imagen']
+    list_display = ['cod_producto','nombre', 'tipo_producto', 'tipo_municion', 'precio', 'stock', 'imagen']
     list_editable = ['tipo_producto', 'tipo_producto', 'tipo_municion', 'precio', 'stock', 'imagen']
-    list_filter = ['nombre', 'tipo_producto', 'tipo_municion']
+    list_filter = ['cod_producto','nombre', 'tipo_producto', 'tipo_municion']
     
 class AdmCarrito(admin.ModelAdmin):
     list_display = ['usuario', 'producto', 'cantidad']
     list_editable = [ 'producto', 'cantidad']
     list_filter = ['usuario', 'producto', 'cantidad']
     
+class AdmPedido(admin.ModelAdmin):
+    list_display = ['id', 'nombre_cliente', 'direccion', 'correo', 'usuario', 'celular', 'region', 'fecha_pedido', 'boleta','estado']
+    list_editable =['nombre_cliente', 'direccion', 'correo', 'usuario', 'celular', 'region', 'fecha_pedido', 'boleta','estado']
+    list_filter = ['id', 'nombre_cliente', 'direccion', 'correo', 'usuario', 'celular', 'region', 'fecha_pedido', 'boleta','estado']
+    
 admin.site.register(Persona,AdmPersona)
 admin.site.register(Usuario,AdmUsuario)
 admin.site.register(Envio,AdmEnvio)
 admin.site.register(Producto,AdmProducto)
+admin.site.register(Pedido,AdmPedido)
+admin.site.register(Carrito,AdmCarrito)
+
+
 
 
 
